61ca89ddff7dc6a0c69a16942982a97f455931d5
Eli Shani <elishani50@gmail.com>
2020-01-15 19:11:49 +0200

    Login page design

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import './LoginPage.css';
+import { Form, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-class LoginPage extends React.Component{
+class LoginPage extends React.Component {
 
-render() {
-  return (
-    <h1>LoginPage</h1>
-  );
-  }
+    render() {
+        return (
+            <div>
+                <h1>LoginPage</h1>
+                <div clasName="p-login">
+                    <div className="p-main">
+                        <h1>Login to documents storage</h1>
+                        <p>or <Link to="/signup">Create a new account</Link></p>
+                        <Form>
+                            <Form.Group controlId="frombsicaEmail">
+                                <Form.Label>Email adderess</Form.Label>
+                                <Form.Control type="email" placeholder="Enter Email" />
+                            </Form.Group>
+                            <Form.Group controlId="frombsicaPassword">
+                                <Form.Label>Pssword</Form.Label>
+                                <Form.Control type="Password" placeholder="Password" />
+                            </Form.Group>
+                            <Button vriant="success" type="button" block>
+                                Login
+                            </Button>
+                        </Form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
 }
 
