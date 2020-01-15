fa40481372e2bdea88babb29bfc5dacd445971bb
Eli Shani <elishani50@gmail.com>
2020-01-15 12:48:58 +0200

    Recreate router  + routers for pages home login and docs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import DocsPage from './pages/DocsPage'
 
-class App extends React.Component{
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 
-render() {
-  return (
-    <h1>App</h1>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/login">
+            <LoginPage />
+          </Route>
+          <Route exact path="/docs">
+            <DocsPage />
+          </Route>
+        </Switch>
+      </Router>
+    );
   }
 
 }
---
+++ src/pages/DocsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DocsPage extends React.Component{
+
+render() {
+  return (
+    <h1>DocsPage</h1>
+  );
+  }
+
+}
+
+export default DocsPage;
