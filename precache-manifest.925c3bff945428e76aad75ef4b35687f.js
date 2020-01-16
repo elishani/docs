825771dc1d9c31dedd12db7a2a8a5cb23cac4f9f
חדר מחשבים פסג''ה <Lab1@pisga.net>
2020-01-16 12:14:10 +0200

    Updates

---
+++ precache-manifest.925c3bff945428e76aad75ef4b35687f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e455a4b94e0814b6933bbb60effcbb14",
+    "url": "/docs/index.html"
+  },
+  {
+    "revision": "8e1ad8d4f969bd7d5850",
+    "url": "/docs/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "abe944b3ba8f645fdcb8",
+    "url": "/docs/static/css/main.ce55f577.chunk.css"
+  },
+  {
+    "revision": "8e1ad8d4f969bd7d5850",
+    "url": "/docs/static/js/2.a799a4fa.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/docs/static/js/2.a799a4fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "abe944b3ba8f645fdcb8",
+    "url": "/docs/static/js/main.556b38e3.chunk.js"
+  },
+  {
+    "revision": "39857adc13681757d5cb",
+    "url": "/docs/static/js/runtime-main.855e0c5b.js"
+  }
+]);
